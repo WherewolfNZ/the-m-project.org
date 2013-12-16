616eed7a1f226528844d4874b7671bdf9edb6acf
rpabon257 hotmail <rpabon257@hotmail.com>
2013-12-16 17:02:19 +0100

    first live version

--- src/js/helper.js
+++ src/js/helper.js
@@ -1,22 +1,25 @@
 // Here you can add your custom js
 
-//smooth scrolling
-$('a').each(function() {
+$(function() {
+
+  //smooth scrolling
+  $('a').each(function() {
     var that = $(this);
     var viewport = $('html, body');
 
     that.on('click', function( e ) {
-        console.log('carajo');
-        var href = that.attr('href');
+      var href = that.attr('href');
 
-        if( href.charAt(0) === '#' && $(href).length > 0 ) {
-            e.preventDefault();
-            if( href === '#' ) {
-                viewport.animate({scrollTop: 0}, 1000);
-            } else {
-                var height = $(href).offset().top;
-                viewport.animate({scrollTop: height}, 1000);
-            }
+      if( href.charAt(0) === '#' && that.length > 0 ) {
+        e.preventDefault();
+        if( href === '#' ) {
+          viewport.animate({scrollTop: 0}, 1000);
+        } else {
+          var height = that.offset().top;
+          viewport.animate({scrollTop: height}, 1000);
         }
+      }
     });
+  });
+
 });
